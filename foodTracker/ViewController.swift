26bbdfe6397504fa9acfa9f5075d3b872002abc5
haseena gul <haseenagul100@gmail.com>
2019-03-18 23:10:29 +0500

    Add tapGesture and textField functionality..

--- foodTracker/ViewController.swift
+++ foodTracker/ViewController.swift
@@ -8,13 +8,92 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController ,UITextFieldDelegate , UIImagePickerControllerDelegate , UINavigationControllerDelegate{
+    
+   
+    
+    //MARK: - properties
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    @IBOutlet weak var mealName: UILabel!
+    
+    @IBOutlet weak var entermealNameTextField: UITextField!
+    
+    @IBOutlet weak var photoimageView: UIImageView!
+    
+    
+    
+    //MARK: - action .
+    
+    @IBAction func setTextButton(_ sender: UIButton) {
+        
+       mealName.text = "Default Text"
     }
+    
+    
+    @IBAction func selectImageFromPhoto(_ sender: UITapGestureRecognizer) {
+        
+        // Hide the keyboard.
+        entermealNameTextField.resignFirstResponder()
+        
+        // UIImagePickerController is a view controller that lets a user pick media from their photo library.
+        let imagePickerController = UIImagePickerController()
+        
+        // Only allow photos to be picked, not taken.
+        imagePickerController.sourceType = .photoLibrary
+        
+        // Make sure ViewController is notified when the user picks an image.
+        imagePickerController.delegate = self
+        present(imagePickerController, animated: true, completion: nil)
+        
+        
+        }
+    
+    
+      override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Handle the text field’s user input through delegate callbacks
+        entermealNameTextField.delegate = self
+        // Make sure ViewController is notified when the user picks an image.
+        
+        
+     
+    }
+                  //MARK: - UITextFieldDelegate
+    
+    
 
+    //methods for textField delegate.
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        //hide the keyboard.
+       textField.resignFirstResponder()
+        return true
+        
+    }
+    
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        
+        entermealNameTextField.text = textField.text
+        
+    }
+    
+    //MARK: - set the UIImage picker  controller delegate method.
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        // Dismiss the picker if the user canceled.
+        dismiss(animated: true, completion: nil)
+    }
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        // The info dictionary may contain multiple representations of the image. You want to use the original.
+       guard let  selectedImage = info[UIImagePickerController.InfoKey.originalImage] as? UIImage else {
+        fatalError("Expected a dictionary containing an image, but was provided the following: \(info)")
+        }
+        
+         // Set photoImageView to display the selected image.
+        photoimageView.image = selectedImage
+        
+        //Dismiss the picker
+        dismiss(animated: true, completion: nil)
+}
 
 }
 
